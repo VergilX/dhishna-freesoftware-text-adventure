4c2236c9602084e8c9142cce91931b81eb54b59b
VergilX <abhinand.dmanoj@gmail.com>
2023-11-10 22:30:08 +0530

    Progress on engine logic
    - Added gitignore
    - src/processing.py: progress on logic
    - story/story.txt: Don't add delimiter in the end

--- game.py
+++ game.py
@@ -3,24 +3,30 @@ Starts the game
 
 Author: @AbhinandDManoj
 """
+from src.processing import generate_story
+
+STORY_SOURCE_DIRECTORY = "story/"
+STORY_FILE = "story.txt"
 
 def main():
     # id set to -1 when game ends
-    id = 0
+    id = 1
 
     # Set terminal color theme
-    
+
 
     # Game loop
     try:
         while (id != -1):
-            id = generate_story(id)
+            file = STORY_SOURCE_DIRECTORY + STORY_FILE
+            id = generate_story(file, id)
     except KeyboardInterrupt:
         # Do you want to exit or not
         pass
-    except Exception:
-        # Modify this to read the error contained in exception
-        print("Story ID not found")
+    """
+    except Exception as e:
+        print(f"Error: {e}")
+        """
 
 if __name__ == "__main__":
     main()
--- src/processing.py
+++ src/processing.py
@@ -1,12 +1,13 @@
 """
 Generates story from database text file
 """
+import ast
 import sys
 
 STORY_SOURCE_DIRECTORY = "../story/"
 SAVE_FILE = f"{STORY_SOURCE_DIRECTORY}/save.txt"
 
-def get_story(file, id):
+def generate_story(file, id):
     """ Read story file and get content of story """
 
     DELIMITER = "`"
@@ -15,7 +16,8 @@ def get_story(file, id):
         # Checking for story ID
         story_found = False
         while not story_found:
-            current_story_id = int(f.readline().split("=")[1])
+            line = f.readline()
+            current_story_id = int(line.split("=")[1])
 
             if current_story_id != id:
                 # Skipping to next story using delimiter
@@ -33,10 +35,22 @@ def get_story(file, id):
                 if story_type == 1:
                     data = {}
                     for line in f:
-                        key, value = line.split("=")
-                        data[key] = value.rstrip()
+                        if line.rstrip() != DELIMITER:
+                            key, value = line.split("=")
+                            data[key] = value.rstrip()
+                        else:
+                            break
 
                     next_story_id = generate_text_story(data)
+
+                elif story_type == 2:
+                    print("Print map")
+                    next_story_id = -1
+                elif story_type == 3:
+                    print("Play minigame")
+                    next_story_id = -1
+                else:
+                    raise Exception("Invalid story type")
 
         return next_story_id
 
@@ -47,7 +61,34 @@ def generate_map():
     pass
 
 def generate_text_story(data):
-    print(data)
+    """ Generates story text, takes choice of user and returns id of next story """
+
+    # Add music, slowness
+    print(data["script"].replace("\\n", "\n"))
+    choices = ast.literal_eval(data["choices"])
+
+    # Change to dynamic choice using curses module
+    choices_limit = 0
+    for choice in choices:
+        choice_no, choice_name = choice.split(",")
+        choices_limit += 1
+
+        print(f"{choice_no}. {choice_name}")
+
+    while True:
+        try:
+            choice = int(input("Your choice: "))
+            if not (0 < choice <= choices_limit):
+                raise ValueError("Invalid range")
+        except ValueError:
+            # Can add funkier replies
+            print("Are you retarded?")
+        else:
+            break
+
+    next_story_id = int(data["next"])
+    return next_story_id
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -57,6 +98,6 @@ if __name__ == "__main__":
         id = int(sys.argv[1])
         file = STORY_SOURCE_DIRECTORY + "story.txt"
         try:
-            get_story(file, id)
+            generate_story(file, id)
         except Exception:
             print("Story not found")
